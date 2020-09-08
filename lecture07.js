9048912d7fd1860bd4684f6d619a228ace9ea762
tlsdbswlek <tlsdbswlek@naver.com>
2020-09-08 16:22:49 +0900

    문자열

---
+++ lecture07.js
@@ -0,0 +1,10 @@
+var a = '작은 따옴표';
+var b = '큰 따옴표';
+var c = '"큰 따옴표';
+var d = '\\역슬래시';
+
+var i = a + b;
+console.log(i);
+
+var practice = '문자열입니다. \n그리고 줄을 바꿔서 다음 문자열입니다.';
+console.log(practice);
